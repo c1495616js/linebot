5b4c8070158de985a9e9c0a0569da91ee455e258
jerrywang761208 <c1495616@gmail.com>
2017-03-24 02:06:01 +0800

    commit

--- loginCnt.js
+++ loginCnt.js
@@ -3,6 +3,7 @@ var cityUrl = 'https://nutncte.herokuapp.com/api/users/getUsers';
 
 function loginCnt(callback){
     axios.get(cityUrl).then((citys)=>{
+        console.log(citys);
         callback(citys);
     }); 
 }
